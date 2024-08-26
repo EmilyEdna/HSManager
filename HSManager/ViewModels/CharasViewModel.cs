e72a79dbda013aed1e82d365d741408f51a9534c
EmilyEdna <847432003@qq.com>
2024-08-26 18:52:25 +0800

    修复BUG

--- HSManager/ViewModels/CharasViewModel.cs
+++ HSManager/ViewModels/CharasViewModel.cs
@@ -4,30 +4,24 @@ using CommunityToolkit.Mvvm.Messaging;
 using HSManager.properties;
 using HSManager.Tools;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
-using XExten.Advance.LinqFramework;
-using static System.Net.WebRequestMethods;
-using XExten.Advance.StaticFramework;
 using System.Windows;
+using System.Windows.Shapes;
+using XExten.Advance.LinqFramework;
 
 namespace HSManager.ViewModels
 {
     public partial class CharasViewModel : ObservableObject
     {
-        public CharasViewModel()
-        {
-            LoadChara();
-        }
-
         [ObservableProperty]
         private ObservableCollection<string> _Pngs;
 
         [RelayCommand]
-        public void Watch(string file)
-        {
-            WeakReferenceMessenger.Default.Send(file);
-        }
+        public void Watch(string file) => WeakReferenceMessenger.Default.Send(file);
 
+        [RelayCommand]
+        public void Load() => LoadChara();
 
         [RelayCommand]
         public void Batch()
@@ -37,13 +31,15 @@ namespace HSManager.ViewModels
             {
                 Task.Run(() =>
                 {
-                    Application.Current.Dispatcher.BeginInvoke(() => {
+                    Application.Current.Dispatcher.BeginInvoke(() =>
+                    {
                         VM.LoadPng(item);
                         VM.ExportPng();
                     });
                 });
             });
-
+            if (!Soft.Default.CharaExportRoute.IsNullOrEmpty())
+                Process.Start("explorer.exe", Soft.Default.CharaExportRoute);
         }
 
         private void EachFolder(string dir, List<string> files)
